2e5314d6ae9da0d1a8ebcc52dfac5b8c24181254
daniellupaca <103132465+daniellupaca@users.noreply.github.com>
2023-11-20 13:42:25 -0500

    Add files via upload

---
+++ StaggeApp_Beta/StageApp/obj/Debug/net6.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("StageApp.MainApplication, StageApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64708bb42554ae714e.MainApplication.class, crc64708bb42554ae714e.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
